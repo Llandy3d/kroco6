f4eeae5741d2e3123e21b84d8342a4de2e9069cd
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-01-28 02:42:45 +0100

    add blockly as package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div id="blocklyDiv" style="height: 480px; width: 1200px;"></div>
+
+
+<script>
+// Import Blockly core.
+import * as Blockly from 'blockly/core';
+// Import the default blocks.
+import * as libraryBlocks from 'blockly/blocks';
+// Import a generator.
+import {javascriptGenerator} from 'blockly/javascript';
+// Import a message file.
+import * as En from 'blockly/msg/en';
+	import { onMount } from 'svelte';
+
+Blockly.setLocale(En);
+const toolbox = {
+  kind: 'flyoutToolbox',
+  contents: [
+    {
+      kind: 'block',
+      type: 'controls_if'
+    },
+    {
+      kind: 'block',
+      type: 'controls_whileUntil'
+    }
+  ]
+};
+
+onMount(() => {
+    const workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
+});
+</script>
+
