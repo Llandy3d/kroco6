bb63b429b0789c6e452e647f04fb53cd01465309
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-01-29 01:49:09 +0100

    scripteditor component

--- src/routes/test/edit/+page.svelte
+++ src/routes/test/edit/+page.svelte
@@ -1,1 +1,18 @@
-HELLOOOO I AM TEST
+<script lang="ts">
+  import * as Tabs from "$lib/components/ui/tabs";
+  import ScriptEditor from "./ScriptEditor.svelte";
+</script>
+
+<Tabs.Root value="blocks">
+  <Tabs.List class="w-full grid grid-cols-2">
+    <Tabs.Trigger value="blocks">Blocks</Tabs.Trigger>
+    <Tabs.Trigger value="script">Script</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="blocks">
+    Make changes to your account here.
+  </Tabs.Content>
+  <Tabs.Content value="script">
+    <ScriptEditor />
+  </Tabs.Content>
+</Tabs.Root>
+
---
+++ src/routes/test/edit/ScriptEditor.svelte
@@ -0,0 +1,13 @@
+<script>
+  import * as Alert from "$lib/components/ui/alert";
+  import { Code } from "lucide-svelte";
+</script>
+
+
+<Alert.Root>
+  <Code class="h-4 w-4" />
+  <Alert.Title>Script to be executed</Alert.Title>
+  <Alert.Description>
+    Who knows, maybe in the future I might allow you to edit it in here :)
+  </Alert.Description>
+</Alert.Root>
