27192a033391b3ffb4f79ce386b6c227226f6ead
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-03-04 02:19:24 +0100

    example scripts

--- src/routes/test/edit/ScriptEditor.svelte
+++ src/routes/test/edit/ScriptEditor.svelte
@@ -2,12 +2,14 @@
   import { loadContent, storeContent } from "$lib/files";
   import type { ScriptFile } from "$lib/stores/editor";
   import * as monaco from "monaco-editor";
+  import { SCRIPT_MAP } from "$lib/example-scripts";
 </script>
 
 <script lang="ts">
   import { onDestroy, onMount } from "svelte";
   import TestToolbar from "./TestToolbar.svelte";
   import { runScriptInCloud, runScriptLocally } from "$lib/backend-client";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 
   export let file: ScriptFile;
 
@@ -42,6 +44,16 @@
     }
   }
 
+  function handleExScript(name: string) {
+    const script = SCRIPT_MAP[name];
+    if (script === undefined) {
+      console.log(`Example script ${name} not found`);
+      return
+    }
+
+    editor.setValue(script);
+  }
+
   onMount(() => {
     script = loadContent(file);
 
@@ -64,5 +76,33 @@
 
 <div class="flex flex-auto flex-col">
   <TestToolbar runTest={runTestLocally} {runTestInCloud} />
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>Script examples</DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Authentication/Authorization</DropdownMenu.Label>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item on:click={() => handleExScript("BASIC_AUTHENTICATION")}>Basic Authentication</DropdownMenu.Item>
+        <DropdownMenu.Item on:click={() => handleExScript("DIGEST_AUTHENTICATION")}>Digest Authentication</DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>API CRUD operations</DropdownMenu.Label>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item on:click={() => handleExScript("CORE_K6_API")}>Core k6 APIs example</DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Cookies</DropdownMenu.Label>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item on:click={() => handleExScript("COOKIES_HEADER")}>Accessing a cookie set in response headers</DropdownMenu.Item>
+        <DropdownMenu.Item on:click={() => handleExScript("COOKIES_LOG_RESPONSE")}>Logging all cookies in response</DropdownMenu.Item>
+        <DropdownMenu.Item on:click={() => handleExScript("COOKIES_SET_JAR")}>Setting a cookie in VU cookie jar</DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Correlation</DropdownMenu.Label>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item on:click={() => handleExScript("EXTRACT_JSON")}>Extracting values from JSON response</DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
   <div class="full-w flex-auto" bind:this={container}></div>
 </div>
