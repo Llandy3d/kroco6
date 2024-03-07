1cd73c2ebf9ee28a6a975c1ef17af12046f069cf
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-03-07 16:13:25 +0100

    Sidebar tweaks

--- src/lib/components/TestList.svelte
+++ src/lib/components/TestList.svelte
@@ -2,20 +2,29 @@
   import type { Test } from "$lib/backend-client";
   import Button from "$lib/components/ui/button/button.svelte";
   import { currentFile, openTestAsFile } from "$lib/stores/editor";
+  import { FileJson2, Layers } from "lucide-svelte";
 
   export let tests: Test[] = [];
   $: tests, console.log(`tests changed in test list:`, tests);
 </script>
 
 <div>
-  <ul role="list" class="text-left">
+  <ul role="list" class="max-h-50 mb-2 overflow-auto text-left">
     {#each tests as test (test.name)}
       <li>
         <button
-          class="text-ellipsis py-1 hover:underline"
+          class="flex items-center gap-1 text-ellipsis py-1 text-sm hover:underline"
           on:click={() => {
             openTestAsFile(test);
-          }}>{test.name}</button
+          }}
+        >
+          {#if test.kind === "Javascript"}
+            <FileJson2 size="12" />
+          {:else}
+            <Layers size="12" />
+          {/if}
+
+          {test.name}</button
         >
       </li>
     {/each}
--- src/routes/SidebarSection.svelte
+++ src/routes/SidebarSection.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <section>
-  <h2 class="text-left font-bold uppercase">{title}</h2>
+  <h2 class="text-left text-sm font-bold uppercase">{title}</h2>
   <slot />
 </section>
