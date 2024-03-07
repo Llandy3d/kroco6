031ecda059b58b5224cdefdbc9d873e95a016adf
Uladzimir Dzmitračkoŭ <3773351+going-confetti@users.noreply.github.com>
2024-03-07 15:12:10 +0100

    Import cloud tests UI update (#42)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api";
+  import { setMode } from "mode-watcher";
+  import { onMount } from "svelte";
   import Editor from "./test/edit/Editor.svelte";
+
+  onMount(() => {
+    setMode("light");
+
+    invoke("close_splashscreen");
+  });
 </script>
 
 <Editor />
