7f36e39f85533a4b06c6d6fa45c31517daf5fa52
Johan Suleiko Allansson <allansson@gmail.com>
2024-03-10 13:16:28 +0100

    fix position while dropping on canvas

--- src/routes/test/edit/blocks/Root.tsx
+++ src/routes/test/edit/blocks/Root.tsx
@@ -1,5 +1,5 @@
 import type { Root as RootType } from "@/lib/stores/blocks/model/loose";
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 
 interface RootProps {
   root: RootType;
@@ -9,7 +9,7 @@ interface RootProps {
 function Root({ root, children }: RootProps) {
   return (
     <div
-      className="relative"
+      className="absolute"
       style={{ left: `${Math.round(root.left)}px`, top: `${Math.round(root.top)}px` }}
     >
       {children}
