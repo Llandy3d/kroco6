c2133b5d26f384e386f7c650ea12876fd8819db9
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-04-28 02:23:06 +0200

    add actions to delete entries by host & path

--- src/routes/test/browser/requests-table-actions.svelte
+++ src/routes/test/browser/requests-table-actions.svelte
@@ -21,6 +21,31 @@
         return !(item.method === entry.method && item.path === entry.path && item.content === entry.content && item.host === entry.host)
       });
     });
+  }
+
+
+  function deleteEntryMultipleByPath() {
+    data.update(items => {
+      // TODO: possibly unselect the item before deletion
+
+      // this sucks but oh well
+      const entry = items.find(item => item.id === id);
+      return items.filter(item => {
+        return !(item.path === entry.path)
+      });
+    });
+  }
+
+  function deleteEntryMultipleByHost() {
+    data.update(items => {
+      // TODO: possibly unselect the item before deletion
+
+      // this sucks but oh well
+      const entry = items.find(item => item.id === id);
+      return items.filter(item => {
+        return !(item.host === entry.host)
+      });
+    });
 
   }
 
@@ -50,6 +75,17 @@
     </DropdownMenu.Group>
     <DropdownMenu.Separator />
     <DropdownMenu.Item on:click={deleteEntryMultiple}>Delete all similar entries</DropdownMenu.Item>
+
+    <DropdownMenu.Separator />
+    <DropdownMenu.Group>
+      <DropdownMenu.Label>Dangerous</DropdownMenu.Label>
+      <DropdownMenu.Item on:click={deleteEntryMultipleByPath}>
+        Delete all entries with same Path
+      </DropdownMenu.Item>
+      <DropdownMenu.Item on:click={deleteEntryMultipleByHost}>
+        Delete all entries with same Host
+      </DropdownMenu.Item>
+    </DropdownMenu.Group>
   </DropdownMenu.Content>
 </DropdownMenu.Root>
 
